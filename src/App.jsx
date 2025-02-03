b0e8e4684873485258721ce7bd37ddaaf82064ec
marco torremacco <mtorremacco@gmail.comm>
2025-02-03 15:19:48 +0100

    feat add 1 milestone

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,17 @@
+import { useState } from "react";
 
+function App() {
+  const [List, setList] = useState(["Gallette", "Pollo", "Yogurt"]);
+
+  return (
+    <div className="container">
+      <h1>Lista Spesa</h1>
+      <ul>
+        {List.map((product, index) => (
+          <li key={index}>{product}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+export default App;
