41a911f30ea2bc6f84fc2798da5103bfc2be26cc
marco torremacco <mtorremacco@gmail.comm>
2025-02-03 15:33:44 +0100

    feat add 3 milestone

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import { useState } from "react";
 
 function App() {
+  const [product, setProduct] = useState("");
   const [List, setList] = useState(["Gallette", "Pollo", "Yogurt"]);
+
+  const sumbit = (e) => {
+    e.preventDefault();
+
+    setList((current) => [...current, product]);
+
+    setProduct("");
+  };
 
   return (
     <div className="container">
@@ -11,6 +20,18 @@ function App() {
           <li key={index}>{product}</li>
         ))}
       </ul>
+      <hr />
+      <h2>Aggiungi Prodotto</h2>
+      <form onSubmit={sumbit}>
+        <input
+          type="text"
+          value={product}
+          onChange={(e) => {
+            setProduct(e.target.value);
+          }}
+        />
+        <button type="submit">Invia</button>
+      </form>
     </div>
   );
 }
